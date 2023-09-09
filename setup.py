05a639a30cf847db90dddfd7ac0bd1df293c4aeb
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2023-09-09 19:12:58 +0200

    add requirements to setup.py so they are installed at installation.
    Bug found by @ZhenyuShi09

--- setup.py
+++ setup.py
@@ -9,6 +9,11 @@ setup(
     author="Nikoleta Glynatsi",
     author_email=("glynatsi@evolbio.mpg.de"),
     packages=find_packages("src"),
+    install_requires=[
+   'numpy',
+   'sympy',
+   'pytest'
+],
     package_dir={"": "src"},
-    description="A package.",
+    description="A package that estimates long-term outcomes and payoffs between a pair of players using a Markov process.",
 )
